02b7e42eb24df795cd4ac6584950df9973d7ae0b
caleb <nascal3@gmail.com>
2026-07-22 17:34:27 +0300

    fix env locations

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,4 @@
-// import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "./schema"
-
-// config({ path: '.env.local' });
 
 const db = drizzle(process.env.DATABASE_URL!, { schema });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
-// import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-// config({ path: '.env.local' });
 
 export default defineConfig({
     out: './db/drizzle',
